5ffd908b958ac98a49f77eabb3b9b67d93127026
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-03-16 16:06:00 +0530

    Refactored code using ES2015 syntax

--- main.js
+++ main.js
@@ -9,61 +9,62 @@ const height = (canvas.height = window.innerHeight);
 // function to generate random number
 
 function random(min, max) {
-	const num = Math.floor(Math.random() * (max - min + 1)) + min;
-	return num;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function Ball(x, y, velX, velY, color, size) {
-	this.x = x;
-	this.y = y;
-	this.velX = velX;
-	this.velY = velY;
-	this.color = color;
-	this.size = size;
-}
-
-Ball.prototype.draw = function() {
-	ctx.beginPath();
-	ctx.fillStyle = this.color;
-	ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-	ctx.fill();
-};
-
-Ball.prototype.update = function() {
-	if (this.x + this.size >= width) {
-		this.velX = -this.velX;
+class Ball {
+	constructor(x, y, velX, velY, color, size) {
+		this.x = x;
+		this.y = y;
+		this.velX = velX;
+		this.velY = velY;
+		this.color = color;
+		this.size = size;
 	}
-
-	if (this.x - this.size <= 0) {
-		this.velX = -this.velX;
+	draw() {
+		ctx.beginPath();
+		ctx.fillStyle = this.color;
+		ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+		ctx.fill();
 	}
+	update() {
+		if (this.x + this.size >= width) {
+			this.velX = -this.velX;
+		}
 
-	if (this.y + this.size >= height) {
-		this.velY = -this.velY;
+		if (this.x - this.size <= 0) {
+			this.velX = -this.velX;
+		}
+
+		if (this.y + this.size >= height) {
+			this.velY = -this.velY;
+		}
+
+		if (this.y - this.size <= 0) {
+			this.velY = -this.velY;
+		}
+
+		this.x += this.velX;
+		this.y += this.velY;
 	}
+	collisionDetect() {
+		for (let j = 0; j < balls.length; j++) {
+			if (!(this === balls[j])) {
+				const dx = this.x - balls[j].x;
+				const dy = this.y - balls[j].y;
+				const distance = Math.sqrt(dx * dx + dy * dy);
 
-	if (this.y - this.size <= 0) {
-		this.velY = -this.velY;
-	}
-
-	this.x += this.velX;
-	this.y += this.velY;
-};
-
-Ball.prototype.collisionDetect = function() {
-	for (let j = 0; j < balls.length; j++) {
-		if (!(this === balls[j])) {
-			const dx = this.x - balls[j].x;
-			const dy = this.y - balls[j].y;
-			const distance = Math.sqrt(dx * dx + dy * dy);
-
-			if (distance < this.size + balls[j].size) {
-				balls[j].color = this.color =
-					'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+				if (distance < this.size + balls[j].size) {
+					balls[j].color = this.color =
+						'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+				}
 			}
 		}
 	}
-};
+}
+
+
+
 
 let balls = [];
 
